34f5513e47c4c56499eac770e6832b6867c55504
wangshujingscan <wangshujing_123@163.com>
2019-10-11 16:50:48 +0800

    增加暴恐批量识别

--- backend/api/violence.py
+++ backend/api/violence.py
@@ -20,7 +20,7 @@ import torchvision.models as models
 # 添加当前项目到环境变量
 import sys
 sys.path.append(os.path.join(os.getcwd(),"backend","api"))
-from util import ProtestDatasetEvalFile, modified_resnet50
+from util import ProtestDatasetEvalFile, ProtestDatasetEvals, modified_resnet50
 import logging
 logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -94,26 +94,129 @@ class violence:
                 }
             except:
                 return {'protest': 0,'violence': 0}
+    
+    def eval_more_file(self,img_paths, model):
+            """
+            return model output of some images
+            """
+            print ("eval_more_file")
+            model.eval()
+            batch_size = len(img_paths)
+            workers = 0
+            cuda = self.cuda
+            timeout = 1
+            # make dataloader
+            try:
+                dataset = ProtestDatasetEvals(img_list = img_paths)
+                print(dataset)
+                data_loader = DataLoader(dataset,
+                                        num_workers = workers,
+                                        batch_size = batch_size,
+                                        timeout= timeout)
+
+                outputs = []
+                imgpaths = []
+
+                n_imgs = len(img_paths)
+                with tqdm(total=n_imgs) as pbar:
+                    for i, sample in enumerate(data_loader):
+                        imgpath, input = sample['imgpath'], sample['image']
+                        if cuda:
+                            input = input.cuda()
+
+                        input_var = Variable(input)
+                        output = model(input_var)
+                        outputs.append(output.cpu().data.numpy())
+                        imgpaths = imgpath
+                        if i < n_imgs / batch_size:
+                            pbar.update(batch_size)
+                        else:
+                            pbar.update(n_imgs%batch_size)
+                
+                
+                dataArr = []
+                arrays = np.concatenate(outputs)
+                # print (arrays)
+                for array in arrays:
+                    dataMap = {}
+                    dataMap['protest'] = array[0]
+                    dataMap['violence'] = array[1]
+                    dataMap['sign'] = array[2]
+                    dataMap['photo'] = array[3]
+                    dataMap['fire'] = array[4]
+                    dataMap['police'] = array[5]
+                    dataMap['children'] = array[6]
+                    dataMap['group_20'] = array[7]
+                    dataMap['group_100'] = array[8]
+                    dataMap['flag'] = array[9]
+                    dataMap['night'] = array[10]
+                    dataMap['shouting'] = array[11]
+                    dataArr.append(dataMap)
+
+                return {"result": dataArr}
+            except:
+                return {'protest': 0,'violence': 0}
                 
     def check_violence(self,img_file):
-    # load trained model
-        # path = os.getcwd()
-        # model_path = 'model_best.pth.tar'
-        # cuda = False
-        # print("*** loading model from {model}".format(model = model_path))
-        # model = modified_resnet50()
-        # if cuda:
-        #     model = model.cuda()
-        
-        # with open(os.path.join(path, 'backend' ,'api', model_path), 'rb') as f:
-        #     model.load_state_dict(torch.load(f ,map_location='cpu')['state_dict'])
-        #     # calculate output
-        # df = eval_one_file(img_file, model)
-        # return df
         return self.eval_one_file(img_file, self.model)
+    
+    def check_violences(self,img_files):
+        return self.eval_more_file(img_files, self.model)
 
 if __name__ == '__main__':
     is_gpu = False
+    img_file = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","BDPAk8S.jpg")
+    img_files = []
+    
+    img_file1 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","auW8xgi.jpg")
+    img_file2 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","bEu0Ihd.jpg")
+    img_file3 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","BgJGB9P.jpg")
+    img_file4 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","BGKV2Qg.jpg")
+    img_file5 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","bGpCUoU.jpg")
+    img_file6 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","bh2DK9Q.jpg")
+    img_file7 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","bhVEAOj.jpg")
+    img_file8 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","BIaBpE7.jpg")
+    img_file9 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","Bid4Z0E.jpg")
+    img_file10 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","biOwhIG.jpg")
+
+    img_file11 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","auW8xgi.jpg")
+    img_file12 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","bEu0Ihd.jpg")
+    img_file13 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","BgJGB9P.jpg")
+    img_file14 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","BGKV2Qg.jpg")
+    img_file15 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","bGpCUoU.jpg")
+    img_file16 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","bh2DK9Q.jpg")
+    img_file17 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","bhVEAOj.jpg")
+    img_file18 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","BIaBpE7.jpg")
+    img_file19 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","Bid4Z0E.jpg")
+    img_file20 = os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","biOwhIG.jpg")
+    
+    img_files.append(img_file1)
+    img_files.append(img_file2)
+    img_files.append(img_file3)
+    img_files.append(img_file4)
+    img_files.append(img_file5)
+    img_files.append(img_file6)
+    img_files.append(img_file7)
+    img_files.append(img_file8)
+    img_files.append(img_file9)
+    img_files.append(img_file10)
+    img_files.append(img_file11)
+    img_files.append(img_file12)
+    img_files.append(img_file13)
+    img_files.append(img_file14)
+    img_files.append(img_file15)
+    img_files.append(img_file16)
+    img_files.append(img_file17)
+    img_files.append(img_file18)
+    img_files.append(img_file19)
+    img_files.append(img_file20)
+
     myviolence = violence(is_gpu)
-    result = myviolence.check_violence(os.path.join(os.getcwd(),"backend","api","yahoo","open_nsfw","images","BDPAk8S.jpg"))
+    t = time.time()
+    startTime = int(round(t * 1000))
+    # result = myviolence.check_violence(img_file)
+    result = myviolence.check_violences(img_files)
+    t = time.time()
+    endTime = int(round(t * 1000))
+    print("cost:"+ str(endTime - startTime) + "ms")
     print (result)
