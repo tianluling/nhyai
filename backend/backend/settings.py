dabb755ac51d3265dab2bbd2b7f4d70d6e481532
wangshujingscan <wangshujing_123@163.com>
2019-10-17 18:27:27 +0800

    修改支持mysql数据库

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,16 +85,25 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ATOMIC_REQUESTS': True
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ATOMIC_REQUESTS': True,
-        'OPTIONS': {
-            'timeout': 20,
-        }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nhyai',
+        'USER': 'root',
+        'PASSWORD': 'rsc123!@#',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
+
 
 
 # Password validation
