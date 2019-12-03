ae2558611c5ebd5fbfa9b51f2b5223b79088974c
wangshujingscan <wangshujing_123@163.com>
2019-12-03 14:43:51 +0800

    取消日志打印

--- backend/backend/settings_task.py
+++ backend/backend/settings_task.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!cex09w=^%pm7k5acx)gcv*ngdtyb-_+w*ngne$2#itkr2(dqv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*',]
 
