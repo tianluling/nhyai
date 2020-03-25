96a8faf1479ac22beaf8ee35045fc5988bb635f8
wangshujingscan <wangshujing_123@163.com>
2020-03-25 11:39:53 +0800

    解决文件像素过高导致内存不足问题

--- backend/api/handwrite/hwconfig.py
+++ backend/api/handwrite/hwconfig.py
@@ -12,8 +12,10 @@ ocrPath  = os.path.join(os.getcwd(),"backend","api","handwrite", "models", "ocr"
 textPath = os.path.join(os.getcwd(),"backend","api","handwrite", "models", "text", "text.weights")
 darkRoot = os.path.join(os.getcwd(),"backend","api","handwrite", "darknet", "libdarknet.so") ##darknet
 TEXT_LINE_SCORE=0.7##text line prob
-scale = 900##可动态修改 no care text.cfg height,width
-maxScale = 1800
+scale = 600##可动态修改 no care text.cfg height,width
+maxScale = 900
 # GPU=True ## gpu for darknet  or cpu for opencv.dnn
 GPU = settings.IS_GPU
 anchors = '16,11, 16,16, 16,23, 16,33, 16,48, 16,68, 16,97, 16,139, 16,198, 16,283'
+if GPU:
+    os.environ['CUDA_VISIBLE_DEVICES']='0'
